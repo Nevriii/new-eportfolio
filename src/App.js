1cdcd019f2109c5e00b16021f4a5a0c40a42b53c
Nevri <vivintejada@gmail.com>
2025-07-01 22:55:04 +0800

    New Committed Files 07/01/25

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar';
+import { Introduction } from './components/Introduction';
+import { Skills } from './components/Skills';
+import Banner from './assets/img/banner-bg.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Introduction />
+      <Skills />
     </div>
   );
 }
---
+++ src/components/Introduction.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myselfImg from "../assets/img/myself.png";
+
+export const Introduction = () => {
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center justify-content-center">
+                    <Col xs={12} md={6} xl={6} className="text-content">
+                        <h1>Hi! I'm Vin</h1>
+                        <p>An aspiring <span className="tagline">Software Engineer</span></p>
+                    </Col>
+                    <Col xs={12} md={6} xl={6} className="image-content">
+                        <img src={myselfImg} alt="myselfImg"></img>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
