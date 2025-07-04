b88c51dcbdd6cfafa4f08b806735a841cc6c4ef3
Nevriii <vivintejada@gmail.com>
2025-07-05 00:15:56 +0800

    Updated Title Logo 07-04-25

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,8 @@
 // import skill1 from "../assets/img/meter1.svg";
 // import skill2 from "../assets/img/meter2.svg";
 // import skill3 from "../assets/img/meter3.svg";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+// import Carousel from 'react-multi-carousel';
+// import 'react-multi-carousel/lib/styles.css';
 // import arrow1 from "../assets/img/arrow1.svg";
 // import arrow2 from "../assets/img/arrow2.svg";
 // import colorSharp from "../assets/img/color-sharp.png"
@@ -36,7 +36,7 @@ export const Skills = () => {
                     <div className="skill-bx wow zoomIn">
                         <h2>Skills</h2>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                        {/* <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider"> */}
                             {/* <div className="item">
                                 <img src={skill1} alt="Image" />
                                 <h5>Web Development</h5>
@@ -53,10 +53,10 @@ export const Skills = () => {
                                 <img src={skill1} alt="Image" />
                                 <h5>Web Development</h5>
                             </div> */}
-                            <div className="item">
+                            {/* <div className="item">
                                 <h5>Placeholder Skill</h5>
-                            </div>
-                        </Carousel>
+                            </div> */}
+                        {/* </Carousel> */}
                     </div>
                 </div>
             </div>
