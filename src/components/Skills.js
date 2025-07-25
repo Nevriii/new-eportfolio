8ad057d761bb24e29d3f255ade23f362a8d84800
Nevriii <vivintejada@gmail.com>
2025-07-26 04:52:54 +0800

    Adjusted Image Rotation

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,20 @@
-// import skill1 from "../assets/img/meter1.svg";
-// import skill2 from "../assets/img/meter2.svg";
-// import skill3 from "../assets/img/meter3.svg";
-// import Carousel from 'react-multi-carousel';
-// import 'react-multi-carousel/lib/styles.css';
-// import arrow1 from "../assets/img/arrow1.svg";
-// import arrow2 from "../assets/img/arrow2.svg";
-// import colorSharp from "../assets/img/color-sharp.png"
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import logo1 from "../assets/img/skills-logo/JavaScriptlogo.png";
+import logo2 from "../assets/img/skills-logo/angularlogo.png";
+import logo3 from "../assets/img/skills-logo/csslogo.png";
+import logo4 from "../assets/img/skills-logo/htmllogo.png";
+import logo5 from "../assets/img/skills-logo/ioniclogo.png";
+import logo6 from "../assets/img/skills-logo/javalogo.png";
+import logo7 from "../assets/img/skills-logo/mysqllogo.png";
+import logo8 from "../assets/img/skills-logo/phplogo.png";
+import logo9 from "../assets/img/skills-logo/pythonlogo.png";
+import logo10 from "../assets/img/skills-logo/reactjslogo.png";
+import logo11 from "../assets/img/skills-logo/springbootlogo.png";
 
 export const Skills = () => {
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5
     },
@@ -35,28 +39,62 @@ export const Skills = () => {
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
                         <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
-                        {/* <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider"> */}
-                            {/* <div className="item">
-                                <img src={skill1} alt="Image" />
-                                <h5>Web Development</h5>
+                        <Container>
+                          <Row className="align-items-center justify-content-center">
+                            <Col xs={12} md={6} xl={6} className="text-content">
+                              <div className="gallery-wrapper container d-flex flex-wrap flex-md-column me-md-5" style={{ paddingRight: "20px" }}>
+                                <div className="gallery">
+                                    <span style={{ "--i": 1 }}>
+                                        <img src={logo6} alt="Java Logo" />
+                                    </span>
+                                    <span style={{ "--i": 2 }}>
+                                        <img src={logo9} alt="Python Logo" />
+                                    </span>
+
+                                    <span style={{ "--i": 3 }}>
+                                        <img src={logo8} alt="PHP Logo" />
+                                    </span>
+                                    <span style={{ "--i": 4 }}>
+                                        <img src={logo7} alt="MySQL Logo" />
+                                    </span>
+                                </div>
+                                <div className="gallery">
+                                    <span style={{ "--i": 5 }}>
+                                        <img src={logo4} alt="HTML5 Logo" />
+                                    </span>
+                                    <span style={{ "--i": 6 }}>
+                                        <img src={logo3} alt="CSS Logo" />
+                                    </span>
+                                    <span style={{ "--i": 7 }}>
+                                        <img src={logo1} alt="JavaScript Logo" />
+                                    </span>
+                                </div>
+                                <div className="gallery">
+                                    <span style={{ "--i": 8 }}>
+                                        <img src={logo10} alt="ReactJS Logo" />
+                                    </span>
+                                    <span style={{ "--i": 9 }}>
+                                        <img src={logo2} alt="Angular Logo" />
+                                    </span>
+                                    <span style={{ "--i": 10 }}>
+                                        <img src={logo5} alt="Ionic Logo" />
+                                    </span>
+                                    <span style={{ "--i": 11 }}>
+                                        <img src={logo11} alt="Spring Boot Logo" />
+                                    </span>
+                                </div>
                             </div>
-                            <div className="item">
-                                <img src={skill2} alt="Image" />
-                                <h5>Brand Identity</h5>
-                            </div>
-                            <div className="item">
-                                <img src={skill3} alt="Image" />
-                                <h5>Logo Design</h5>
-                            </div>
-                            <div className="item">
-                                <img src={skill1} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div> */}
-                            {/* <div className="item">
-                                <h5>Placeholder Skill</h5>
-                            </div> */}
-                        {/* </Carousel> */}
+                            </Col>
+                            <Col xs={12} md={6} xl={6} className="text-content">
+                              <p>
+                                I'm a Computer Science Student at Mapúa University specializing in Application Development.
+                                Throughout my journey as a student, I have developed projects that uses Java, JavaScript,
+                                Python, HTML, CSS, PHP, and MySQL. I also have experience using frameworks such as ReactJS,
+                                Angular, Ionic, and Spring Boot.
+                              </p>
+                            </Col>
+                          </Row>
+                        </Container>
                     </div>
                 </div>
             </div>
