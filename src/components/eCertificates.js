e19a5d4a53637cedc1b7719ba88a2daa39e6a0de
Nevriii <vivintejada@gmail.com>
2025-07-26 21:50:19 +0800

    started e-cert

---
+++ src/components/eCertificates.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+export const eCertificates = () => {
+    return (
+        <section className="e-certificates" id="e-certificates">
+            <div className="container">
+                <div className="row">
+                <div className="col-12">
+                    <div className="e-certificates-bx wow zoomIn">
+                    <h2>eCertificates</h2>
+                    {/* Content for eCertificates will go here */}
+                    </div>
+                </div>
+                </div>
+            </div>
+        </section>
+    );
+}
