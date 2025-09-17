b508059d590585d1bef665474b9c48af9d163d3f
Nevri <vivintejada@gmail.com>
2025-09-18 02:30:03 +0800

    Update Committed Files (Di pa tapos ang Project imgbx part)

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
 import { ECertificates } from './components/ECertificates';
 import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Projects />
       <ECertificates />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 text-center">
+                        <p>© 2025 Vin Tejada. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
